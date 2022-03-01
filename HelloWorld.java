3c2457a0149398c9c3d3246c6eba1d7c3d55a996
nrothe1998 <nrothe1998@gmail.com>
2022-03-01 15:24:07 +0530

    commiting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,12 @@
 public class HelloWorld{
 	
-	public static void main(String [] args){}
+	public static void main(String [] args){
 
 		System.out.println("Hello world");
 		int a=10;
 		int b=20;
 		String name="Nilesh";
 		
-
+		
+}
 }
